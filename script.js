bb4f793f72d50acdce90d13583e6fee24c3d7a2e
said <saidjrhussin2@gmail.com>
2025-09-30 19:16:09 +0800

    complete work

--- script.js
+++ script.js
@@ -1,32 +1,40 @@
-const container = document.querySelector('.container');
-const gridButton = document.createElement('button');
-const big = document.querySelector('.big');
+const big = document.querySelector(".big");
+const container = document.querySelector(".container");
+const gridButton = document.createElement("button");
 
-gridButton.textContent = "Change Number of Grid";
 big.insertBefore(gridButton, big.children[1]);
+gridButton.textContent = "Change grid size";
 
-gridButton.addEventListener('click', () =>{
-    const size = parseInt(prompt("Choose grid size (1-64):"));
-    if (isNaN(size) || size <1 || size > 64){
-        alert("Please enter valid grid size");
+gridButton.addEventListener('click', ()=>{
+    const size = parseInt(prompt("Enter Size (1-64): "));
+    if (isNaN(size) || size < 1 || size> 64){
+        alert("Please Enter valid size");
+        return;
     }
-    createGrid(size);
+    changeSize(size);
 });
 
-createGrid(16);
-function createGrid(size){
+changeSize(16);
+
+function changeSize(size){
     container.innerHTML = "";
     const squareSize = 700/size;
 
-    for (let i = 0; i < size * size; i++){
-    const square = document.createElement('div');
-    square.classList.add("square");
-    square.style.width = `${squareSize}px`;
-    square.style.height = `${squareSize}px`;
+    for (let i =0; i< size * size; i++){
+        const square = document.createElement("div");
+        square.classList.add("square");
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
 
-    square.addEventListener('mouseenter', ()=>{
-        square.classList.add("hovered");
-    });
-    container.appendChild(square);
-}
+
+        square.addEventListener('mouseenter', () =>{
+            
+                const r = Math.floor(Math.random() * 255);
+                const g = Math.floor(Math.random() * 255);
+                const b = Math.floor(Math.random() * 255);
+                square.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+        });
+
+        container.appendChild(square);
+    }
 }
